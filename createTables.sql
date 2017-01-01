697244fe1ef3ab1d06d652f9a35c89bcc598c05d
AlllllllllenZhang <zky2358029599@live.com>
2017-01-01 18:39:09 +0800

    createTables
    describe how to build the tables of database.

---
+++ createTables.sql
@@ -0,0 +1,26 @@
+use XLZX
+go
+create table Visiter(
+ID int primary key not null,
+visitAccount varchar(20) not null,
+visitPassword varchar(40) not null,
+visitName varchar(20) not null,
+visitPhone varchar(15) not null,
+visitAddress varchar(100) not null,
+visitSex varchar(6) not null,
+visitBeizhu varchar(100)
+)
+go
+create table Admins(
+adminAccount varchar(20) primary key not null,
+adminPassword char(20) not null,
+adminAuthority varchar(10) not null
+)
+go
+create table Article(
+ID int primary key not null,
+title varchar(20) not null,
+addr varchar(50) not null,
+otherInfo varchar(50) not null
+)
+go
